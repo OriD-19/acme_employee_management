68499073320e291582066856ecb9164830d08e58
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-07-23 22:38:25 -0600

    fix: modified vacation event description

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from src.menu.app import App
 from src.employee.queries import EmployeeInMemoryStore
 from src.store.store import store
 
+
 def main():
     # choose employee storing strategy
     s = EmployeeInMemoryStore(store)
 
     app = App(s)
     app.run()
-
 
 
 if __name__ == "__main__":
